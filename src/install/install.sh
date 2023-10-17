c92aab9ae6c54cb84cffcd947b68d8a1f0ed20e2
robshape <robshape@users.noreply.github.com>
2023-10-17 13:57:51 +0200

    feat: Use battery charging image from upstream

--- src/install/install.sh
+++ src/install/install.sh
@@ -15,6 +15,7 @@ if [ ! -f $FLAG_PATH ]; then
 	mkdir -p $BAK_PATH
 	cp /misc/boot_logo.bmp.gz $BAK_PATH
 	cp /misc/kernel.dtb $BAK_PATH
+	cp /misc/ramdisk.img $BAK_PATH
 fi
 
 was_updated() {
@@ -25,6 +26,11 @@ was_updated() {
 
 		if [[ "$A_NAME" == "boot_logo.bmp.gz" ]]; then
 			# we don't care if the user has changed their boot logo
+			continue
+		fi
+
+		if [[ "$A_NAME" == "charging.png" ]]; then
+			# we don't care if the user has changed their charging image
 			continue
 		fi
 
@@ -47,11 +53,18 @@ if [ ! -f $FLAG_PATH ] || was_updated; then
 	echo "updating misc partition"
 	mount -o remount,rw /dev/block/actb /misc
 	cp $SYSTEM_PATH/dat/dmenu.bin /misc
+	cp $SYSTEM_PATH/dat/kernel.dtb /misc
+	cp $SYSTEM_PATH/dat/ramdisk.img /misc
+
+	# boot logo, only installed, never updated
 	if [ ! -f $FLAG_PATH ]; then
-		# only replace boot logo on install not update!
 		cp $SYSTEM_PATH/dat/boot_logo.bmp.gz /misc
 	fi
-	cp $SYSTEM_PATH/dat/kernel.dtb /misc
+	# charging graphic, same
+	if [ ! -f $SYSTEM_PATH/dat/charging.png ]; then
+		cp $SYSTEM_PATH/dat/charging.png /misc
+	fi
+
 	touch $FLAG_PATH
 	sync && reboot
 fi
